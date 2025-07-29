a8029142f647a9b16f58003536f3ec2f354658ee
Hanan <your-email@example.com>
2025-07-29 10:47:28 -0700

    Task 5: Fix Pill component to accept and render title prop dynamically

---
+++ alx-project-0x00/interfaces/index.tsx
@@ -0,0 +1,12 @@
+export interface PillProps {
+  title: string;
+}
+
+export interface CardProps {
+  title: string;
+  location: string;
+  price: string;
+  rating: number;
+  imageUrl: string;
+  tags: string[];
+}
